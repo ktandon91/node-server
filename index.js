50269587e2a85bad7cf9924b0f93f0cff8b0885d
Karan <karantandon1991@gmail.com>
2020-05-09 18:43:11 +0530

    oauth with node, passport and google-oauth

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express'); // common js modules
-
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/keys');
 // import is es 2015 syntax not available in node
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    // console.log(res)
-    // console.log(req)
-    res.send({message:"sanju, I love you!"})
-});
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
